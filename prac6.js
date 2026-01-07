4a01a1c534c735e828564b657b6647a4a65dc4e6
imprintnext-karma <kenton@imprintnext.com>
2026-01-07 18:30:03 +0530

    Reverse a string without using built-in methods.

--- prac6.js
+++ prac6.js
@@ -13,27 +13,35 @@
 // console.log(count)
 
 /////////BEST SOLUTION///////////
+// const str = "Hello imprintnext";
+// const vowels = ['a','e','i','o','u'];
+
+// let vowelCount = 0;
+// let consonantCount = 0;
+
+// for (let char of str.toLowerCase()) {
+//   if (char >= 'a' && char <= 'z') {
+//     if (vowels.includes(char)) {
+//       vowelCount++;
+//     } else {
+//       consonantCount++;
+//     }
+//   }
+// }
+
+// console.log("Vowels:", vowelCount);
+// console.log("Consonants:", consonantCount);
+
+//Reverse a string without using built-in methods.
+
 const str = "Hello imprintnext";
-const vowels = ['a','e','i','o','u'];
+const arrStr = str.split('');
+let newChar = '';
 
-let vowelCount = 0;
-let consonantCount = 0;
-
-for (let char of str.toLowerCase()) {
-  if (char >= 'a' && char <= 'z') {
-    if (vowels.includes(char)) {
-      vowelCount++;
-    } else {
-      consonantCount++;
-    }
-  }
+for(let i = arrStr.length -1; i >= 0 ; i--){
+    newChar += arrStr[i]
 }
-
-console.log("Vowels:", vowelCount);
-console.log("Consonants:", consonantCount);
-
-
-
+console.log(newChar);
 
 
 
