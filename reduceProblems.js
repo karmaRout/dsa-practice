7494ef5dc4c1d9ebde9bae43031306e342c25163
imprintnext-karma <kenton@imprintnext.com>
2026-01-08 11:48:01 +0530

    Find the maximum number in an array.

--- reduceProblems.js
+++ reduceProblems.js
@@ -3,9 +3,16 @@ const arr = [1,2,3,4,5];
 const result = arr.reduce((acc,item)=>{
     return acc + item
 },0)
-console.log("RESULT :-", result)
+//console.log("RESULT :-", result)
 //////SYNTAX/////////
 // array.reduce((accumulator, currentItem) => {
 //   return newAccumulator;
 // }, initialValue);
 //////////////////////
+
+/////////////////////////////Find the maximum number in an array.////////////////////////////////
+const arr1 = [1,2,10,5,45,23,12]
+const result1 = arr1.reduce((acc, item)=>{
+    return acc > item ? acc : item
+},arr1[0]) 
+console.log(result1)
